9d151bf0ae5babdb2cc7860f44a1f118dada705f
titetil <gtetil@us.tiauto.com>
2019-10-21 07:39:08 -0400

    - Finished initial debug.  Made a few mods.  Looks good so far.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <avr/interrupt.h>
 #include <avr/sfr_defs.h>
 #include <avr/sleep.h>
+#include <util/delay.h>
 
 // digital_inputs
 #define DI_PORT				PORTB
@@ -24,12 +25,28 @@
 uint8_t ignition_state = 0;
 uint8_t sleep_flag = 0;
 
+void sleep(void) {
+	
+	cli(); // Disable interrupts
+	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+	sleep_enable();
+	sleep_bod_disable();
+	sei();
+	sleep_cpu();
+	// the AVR now sleeps until IGN_INPUT turns on...
+	sleep_disable();
+	sei(); // enable interrupts
+	
+}
 
 ISR(PCINT0_vect) {  // IGN_INPUT vector
 	
 	if (bit_is_clear(DI_PIN, IGN_INPUT)) {  // check if IGN_INPUT is ON (this input is reverse logic)
-		ignition_state = 1;
-		DO_PORT |= (1 << PWR_ON_OUTPUT);  // turn on PWR_ON_OUTPUT (enables power supply to RPi)
+		_delay_ms(500);  // debounce
+		if (bit_is_clear(DI_PIN, IGN_INPUT)) {
+			ignition_state = 1;
+			DO_PORT |= (1 << PWR_ON_OUTPUT);  // turn on PWR_ON_OUTPUT (enables power supply to RPi)
+		}
 	}
 	else {
 		ignition_state = 0;
@@ -37,22 +54,24 @@ ISR(PCINT0_vect) {  // IGN_INPUT vector
 	
 }
 
-ISR(PCINT2_vect) {  // RPI_INPUT vector
+ISR(INT0_vect) {  // RPI_INPUT vector
 	
-	if (bit_is_clear(DI_PIN, RPI_INPUT)) {  // check in RPI_INPUT is OFF		
+	if (bit_is_set(DI_PIN, RPI_INPUT)) {  // check in RPI_INPUT is OFF	
 		if (ignition_state == 0) {
 			DO_PORT &= ~(1 << PWR_ON_OUTPUT);  // turn off PWR_ON_OUTPUT (disables power supply to RPi)
-			sleep_flag = 1;
+			sleep();			
 		}
 	}
 	
 }
 
-void initPCinterrupts(void) {
+void initInterrupts(void) {
 	
 	GIMSK |= (1 << PCIE);   // pin change interrupt enable
+	GIMSK |= (1 << INT0);   // pin change interrupt enable
 	PCMSK |= (1 << PCINT0); // pin change interrupt enabled for IGN_INPUT (PCINT0)
-	PCMSK |= (1 << PCINT2); // pin change interrupt enabled for RPI_INPUT (PCINT2)
+	MCUCR |= (1 << ISC00);  // set for falling edge
+	MCUCR |= (1 << ISC01);  // set for falling edge
 	sei();                  // enable interrupts
 	
 }
@@ -65,24 +84,10 @@ int main(void)
 	
 	DO_PORT_DDR |= (1 << PWR_ON_OUTPUT);  // set PWR_ON_OUTPUT pin as output
 	
-	initPCinterrupts();
+	initInterrupts();
+	sleep();  // put to sleep right away
 	
     while (1) {
-		
-		if (sleep_flag) {
-			
-			sleep_flag = 0;
-			cli(); // Disable interrupts
-			set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-			sleep_enable();
-			sleep_bod_disable();
-			sei();
-			sleep_cpu();
-			// the AVR now sleeps until IGN_INPUT turns on...
-			sleep_disable();
-			sei(); // enable interrupts
-			
-		}
 		
     }
 }
